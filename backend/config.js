4b7490381e76b66253580b3c18db80b91920e0f5
Gaxathri <sgayathrisds@gmail.com>
2025-02-19 20:55:28 +0530

    Updated Modified files

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://Gayathri:rAQW6IKX7HzPSzf5@clusterbookstore.64pfb.mongodb.net/books?retryWrites=true&w=majority&appName=Clusterbookstore";
+  "mongodb+srv://sgayathrisds:EWcgxXhXvrVs5P2P@clusterbookstore.znjnj.mongodb.net/books?retryWrites=true&w=majority&appName=Clusterbookstore";
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
