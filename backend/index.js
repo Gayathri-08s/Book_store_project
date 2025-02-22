54fcf6396d39bf577c578b362b0f0d3dcf46ebb5
Gaxathri <sgayathrisds@gmail.com>
2025-02-22 12:40:45 +0530

    Fixed cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 app.use(cors({
-  origin: [APP_URL, FRONTEND_URL],
+  origin: "*",
   credentials: true,
   methods: ["GET", "HEAD", "PATCH", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
